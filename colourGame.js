1a134c04920b535c61d6476e3069bb9858051302
Ayush Khanna <ayushkhanna1996@gmail.com>
2018-07-29 16:36:25 +0530

    working perfectly

--- colourGame.js
+++ colourGame.js
@@ -1,16 +1,25 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-]
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = randomColor();
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click",function(){
+	//generate all new colours
+	colors = generateRandomColors(6);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//set h1's rgb to new one
+	colorDisplay.textContent = pickedColor;
+	//change color of squares
+	for(var i = 0; i < squares.length; i++){
+		//add colours to squares
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -26,7 +35,9 @@ for(var i = 0; i < squares.length; i++){
 		//compare colour from pickedColour
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "CORRECT";
+			resetButton.textContent = "Play Again?"
 			changeColor(clickedColor);
+			h1.style.background = clickedColor;
 		} else{
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
@@ -42,7 +53,28 @@ function changeColor(color){
 	}
 }
 
-function randomColor(){
+function pickColor() {
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make an array
+	var arr = [];
+	//repeat num times
+	for(i = 0; i < num; i++){
+		//get random color and push into array
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor(){
+	//random red blue green colour
+	var r = Math.floor(Math.random()*256);
+	var b = Math.floor(Math.random()*256);
+	var g = Math.floor(Math.random()*256);
+
+	//return "rgb(r,g,b)" and we need to break in it string form
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
